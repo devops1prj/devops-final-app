32cbeeea3f5b6411799ee974cb1a8c54700bbf5c
Praveen-m-cyber <1dt23mc052@dsatm.edu.in>
2025-08-13 19:05:41 +0530

    fix: Update frontend API endpoint to full URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ function App() {
       <h1> My first deployment</h1>
       <h1>praveen</h1>
       <h2>Dsatm</h2>
-      <h6>mee and me</h6>
       <p>This frontend is served by a React container.</p>
       <h2>Message from Backend:</h2>
       <p style={{ color: '#3498db', fontSize: '1.5em', fontWeight: 'bold' }}>{message}</p>
