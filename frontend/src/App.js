edba90c67169d6da062503d1b6459d12e13234c7
Praveen-m-cyber <1dt23mc052@dsatm.edu.in>
2025-08-18 10:19:15 +0530

    Your commit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ function App() {
       <h1> My first deployment</h1>
       <h1>praveen</h1>
       <h2> Mallela Praveen Kumar</h2>
+      <h1> praveen</h1>
       <p>This frontend is served by a React container.</p>
       <h2>Message from Backend:</h2>
       <p style={{ color: '#3498db', fontSize: '1.5em', fontWeight: 'bold' }}>{message}</p>
