ede2136a205dc47d379ac0dd3e61f12a949e7b2a
jayanth kumar P N <jayanthkumarpn@192.168.1.2>
2025-12-03 19:59:28 +0530

    Test: Update frontend title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,8 @@ function App() {
       <h1> SST tech</h1>
       <h1> mallela</h1>
       <h1>praveen</h1>
-      <h2>final changes</h2>
+      <h2>final changes1</h2>
+      <h1> Jayanth </h1>
       <p>This frontend is served by a React container.</p>
       <h2>Message from Backend:</h2>
       <p style={{ color: '#3498db', fontSize: '1.5em', fontWeight: 'bold' }}>{message}</p>
