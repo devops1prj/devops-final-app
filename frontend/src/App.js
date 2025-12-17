a8800697ad18f4ca87a57f6fe78af678919a3db6
jayanth kumar P N <jayanthkumarpn@Apples-MacBook-Air.local>
2025-12-17 06:53:05 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
       <h1>Simple Full-Stack App</h1>
       <h1> My first deployment</h1>
-      <h1>Jayanth</h1>
+      <h1>Jayanth.</h1>
       <h2> Jayanth Kumar P N</h2>
       <h1> Jayanth</h1>
       <h1> devops</h1>
@@ -21,7 +21,7 @@ function App() {
       <h2>final changes1</h2>
       <h1> jayanth </h1>
       <h1>dsatm</h1>
-      <h2> ***** </h2>
+      <h2> ****** </h2>
       <p>This frontend is served by a React container.</p>
       <h2>Message from Backend:</h2>
       <p style={{ color: '#3498db', fontSize: '1.5em', fontWeight: 'bold' }}>{message}</p>
