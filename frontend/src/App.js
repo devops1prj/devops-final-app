a98b13fb79161fa4bcab210eccbe16fe4fd6177f
jayanth kumar P N <jayanthkumarpn@192.168.1.11>
2026-01-18 13:33:54 +0530

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,17 +11,18 @@ function App() {
     <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
       <h1>Simple Full-Stack App</h1>
       <h1> My first deployment</h1>
-      <h1>Jayanth.</h1>
+      <h1>Jayanth</h1>
       <h2> Jayanth Kumar P N</h2>
       <h1> Jayanth</h1>
       <h1> devops</h1>
-      <h1> SST tech</h1>
+      <h1> SS tech</h1>
       <h1> Kumar</h1>
       <h1>Jayanth</h1>
       <h2>final changes1</h2>
       <h1> jayanth </h1>
       <h1>dsatm</h1>
-      <h2> @******@ </h2>
+      <h2> ******@ </h2>
+      <h1>hello </h1>
       <p>This frontend is served by a React container.</p>
       <h2>Message from Backend:</h2>
       <p style={{ color: '#3498db', fontSize: '1.5em', fontWeight: 'bold' }}>{message}</p>
